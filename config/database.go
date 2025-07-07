6a361e2bf12c06f962af2057d5fa2ee369218a69
tavo <tavo@tavo.one>
2025-07-07 13:01:45 -0600

    enforce foreign keys and WAL journal

--- config/database.go
+++ config/database.go
@@ -12,6 +12,14 @@ func InitDB(connDvr, connStr string) (*sql.DB, error) {
 		return nil, err
 	}
 
+	if _, err := db.Exec("PRAGMA foreign_keys = ON"); err != nil {
+		return nil, err
+	}
+
+    if _, err := db.Exec("PRAGMA journal_mode = WAL"); err != nil {
+        return nil, err
+    }
+
 	if err := db.Ping(); err != nil {
 		return nil, err
 	}
