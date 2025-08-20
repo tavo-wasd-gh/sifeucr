78b58e7b3567156059f843f431182b1105708ecb
tavo <tavo@tavo.one>
2025-08-20 00:44:03 -0600

    auto-create database file and exec schema if the file does not exist on startup

--- config/database.go
+++ config/database.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "modernc.org/sqlite"
 )
@@ -11,6 +13,11 @@ const (
 )
 
 func InitDB(dbFile string) (*sql.DB, bool, error) {
+	dbFileNonExistent := false
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		dbFileNonExistent = true
+	}
+
 	db, err := sql.Open(DEFAULT_DRIVER, dbFile)
 	if err != nil {
 		return nil, false, err
@@ -24,15 +31,35 @@ func InitDB(dbFile string) (*sql.DB, bool, error) {
 		return nil, false, err
 	}
 
+	if err := db.Ping(); err != nil {
+		return nil, false, err
+	}
+
+	if dbFileNonExistent {
+		if err := applySchema(db, schema); err != nil {
+			_ = db.Close()
+			return nil, false, err
+		}
+	}
+
 	var firstTimeSetup bool
 	err = db.QueryRow("SELECT NOT EXISTS (SELECT 1 FROM users)").Scan(&firstTimeSetup)
 	if err != nil {
 		return nil, false, err
 	}
 
-	if err := db.Ping(); err != nil {
-		return nil, false, err
-	}
-
 	return db, firstTimeSetup, nil
+}
+
+func applySchema(db *sql.DB, ddl string) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Exec(ddl); err != nil {
+		_ = tx.Rollback()
+		return fmt.Errorf("error applying schema: %w", err)
+	}
+	// _, _ = tx.Exec(`PRAGMA user_version = 1`)
+	return tx.Commit()
 }
